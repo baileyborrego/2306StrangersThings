c1bc19e2f4b372befcddad4912bb32c291c0e5f3
baileyborrego <baileyborrego@gmail.com>
2023-09-21 10:08:10 -0600

    fetchToken added to index

--- src/API/index.js
+++ src/API/index.js
@@ -5,9 +5,33 @@ export const fetchAllPosts = async () => {
     try {
         const response = await fetch (`${BASE_URL}/posts`);
         const allPosts = await response.json();
-        console.log(allPosts);
+        // console.log(allPosts);
         return allPosts
     } catch (err) {
         console.error('Uh oh, trouble fetching posts!', err);
     }
 };
+
+export const fetchToken = async (username, password) => {
+  
+    try {
+      const response = await fetch(`${BASE_URL}/users/registerLogin`, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            user: {
+              username: `${username}`,
+              password: `${password}`
+            }
+        })
+      });
+        const result = await response.json();
+        console.log(username, password);
+        sessionStorage.token = result.data.token;
+        return localStorage.token;
+    } catch (err) {
+      console.error(err);
+    }
+  }
