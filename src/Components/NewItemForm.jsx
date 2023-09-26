124ac03ac76112646fe86a2b78549b9346612bf7
sawyerno <sawyernoland2005@gmail.com>
2023-09-26 10:42:32 -0400

    99% done

--- src/Components/NewItemForm.jsx
+++ src/Components/NewItemForm.jsx
@@ -1,5 +1,29 @@
-import { useState, useEffect } from "react";
+// mostly done i think, may be a slight work in progress still
+const COHORT_NAME = '2306-FTB-ET-WEB-AM';
+const BASE_URL = `https://strangers-things.herokuapp.com/api/${COHORT_NAME}`;
 
-export default function AllPosts() {
-  const [posts, setPosts] = useState()
+const makePost = async () => {
+  try {
+    const response = await fetch(`${BASE_URL}/posts`, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${token}`
+      },
+      body: JSON.stringify({
+        post: {
+          title: title,
+          description: description,
+          price: price,
+          location: location,
+          willDeliver: true
+        }
+      })
+    });
+    const result = await response.json();
+    console.log(result);
+    return result
+  } catch (error) {
+    console.error(error);
+  }
 }
